32b859d69af993c56f1b88f7598377d4f4158ca8
Hakan Kaya <hakan_kaya54@hotmail.com>
2022-11-27 02:39:04 +0100

    added first consumer

---
+++ consumer1.js
@@ -0,0 +1,14 @@
+const getData = async () => {
+    const response = await fetch('http://localhost:8080/getCustomerData');
+    const json = await response.json();
+    return json;
+}
+
+const printCustomers = async () => {
+    const data = await getData();
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i].name, data[i].email, data[i].status)
+    }
+}
+
+printCustomers()
