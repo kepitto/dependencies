bcba7a69be39f167d91aab27bb18489a037a5bd1
Hakan Kaya <hakan_kaya54@hotmail.com>
2022-11-27 02:18:08 +0100

    json return complete

--- customerservice.js
+++ customerservice.js
@@ -9,7 +9,7 @@ const client = new Client({
   host: "postgres",
 });
 
-const createTable = async (id) => {
+const createCustomerTable = async (id) => {
   try {
       await client.query(
           `CREATE TABLE IF NOT EXISTS customer
@@ -17,6 +17,7 @@ const createTable = async (id) => {
               id SERIAL,
               name text,
               address text,
+              birthdate text,
               email text,
               status text,
               CONSTRAINT customer_pkey PRIMARY KEY (id)
@@ -24,6 +25,55 @@ const createTable = async (id) => {
       return true;
   } catch (error) {
       console.log("Table creation failed");
+      console.error(error.stack);
+      return false;
+  }
+};
+
+const createProductTable = async (id) => {
+  try {
+      await client.query(
+          `CREATE TABLE IF NOT EXISTS products
+          (
+              id SERIAL,
+              customer_id int,
+              name text,
+              balance int,
+              productcode text,
+              interestrate text,
+              CONSTRAINT products_pkey PRIMARY KEY (id)
+          );`); 
+      return true;
+  } catch (error) {
+      console.log("Table creation failed");
+      console.error(error.stack);
+      return false;
+  }
+};
+
+const insertCustomer = async (name, address, birthdate, email, status) => {
+  try {
+      await client.query(
+          `INSERT INTO customer (name, address, birthdate, email, status)  
+          VALUES ($1, $2, $3, $4, $5)`, [name, address, birthdate, email, status]);
+      console.log("added data");
+      return true;
+  } catch (error) {
+      console.log("Data could not be added");
+      console.error(error.stack);
+      return false;
+  }
+};
+
+const insertProduct = async (customer_id, name, balance, productcode, interestrate) => {
+  try {
+      await client.query(
+          `INSERT INTO products (customer_id, name, balance, productcode, interestrate)  
+          VALUES ($1, $2, $3, $4, $5)`, [customer_id, name, balance, productcode, interestrate]);
+      console.log("added data");
+      return true;
+  } catch (error) {
+      console.log("Data could not be added");
       console.error(error.stack);
       return false;
   }
@@ -43,10 +93,56 @@ app.get("/customer", async (req, res) => {
   res.send(JSON.stringify(results));
 });
 
+app.get("/product", async (req, res) => {
+  const results = await client
+    .query("SELECT * FROM products")
+    .then((payload) => {
+      return payload.rows;
+    })
+    .catch(() => {
+      throw new Error("Query failed");
+    });
+  res.setHeader("Content-Type", "application/json"); 
+  res.status(200);
+  res.send(JSON.stringify(results));
+});
+
+app.get("/getCustomerData", async (req, res) => {
+  const results = await client
+    .query("SELECT * FROM customer")
+    .then((payload) => {
+      return payload.rows;
+    })
+    .catch(() => {
+      throw new Error("Query failed");
+    });
+  for (let i = 0; i < results.length; i++) {
+    results[i].products = await client
+    .query("SELECT products.name, products.balance FROM products WHERE products.customer_id = " + results[i].id)
+    .then((payload) => {
+     return payload.rows;
+    })
+    .catch(() => {
+     throw new Error("Query failed");
+    });
+  }
+  console.log(results);
+  res.setHeader("Content-Type", "application/json"); 
+  res.status(200);
+  res.send(JSON.stringify(results));
+});
+
+
 (async () => {
   await client.connect();
-  createTable();
-
+  createCustomerTable();
+  createProductTable();
+  insertCustomer("Hakan", "Adresse", "xx-xx-xxxx" ,"Email-Adresse", "Status");
+  insertCustomer("Günther", "Adresse", "xx-xx-xxxx" ,"Email-Adresse", "Status");
+  insertProduct("1", "Staubsauger", "700", "91203912" , "2");
+  insertProduct("1", "Staubsauger", "700", "91203912" , "2");
+  insertProduct("2", "Backofen", "700", "91203912" , "3");
+  
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
